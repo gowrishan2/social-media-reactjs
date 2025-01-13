da9eeeb825466a6850412cc2604b5f80dc4ccc72
gowrishan2 <gowrishangararl@gmail.com>
2025-01-13 22:18:29 +0530

    About updated

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <main className='About'>
         <h2>About</h2>
-        <p style={{marginTop: '1rem'}}>Hi, this is a social app</p>
+        <p style={{marginTop: '1rem'}}>This is a Social Media Website. You can post your memories here...</p>
     </main>
   )
 }
--- src/Missing.js
+++ src/Missing.js
@@ -6,6 +6,7 @@ const Missing = () => {
         <h2>Page Not Found</h2>
         <p>Well, that's disappointing</p>
         <p>Visit our Home Page</p>
+        
     </main>
   )
 }
