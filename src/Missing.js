2fe023c725efaf65f5211c4c8fe8b8821be7b664
gowrishan2 <gowrishangararl@gmail.com>
2025-01-18 20:56:12 +0530

    missing updated

--- src/Missing.js
+++ src/Missing.js
@@ -4,7 +4,7 @@ const Missing = () => {
   return (
     <main className='Missing'>
         <h2>Page Not Found</h2>
-        <p>Visit our Home Page</p>
+        <p>Click here to Home Page</p>
         
     </main>
   )
